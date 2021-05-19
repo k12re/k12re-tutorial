99c65d1e3207e41d817a599446169b01e2525119
k12re <83732570+k12re@users.noreply.github.com>
2021-05-19 22:22:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/image.png') {
+        myImage.setAttribute('src','images/image2.png');
+    }   else {
+        myImage.setAttribute('src','images/Image.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+myButton.onclick = function() {
+    setUserName();
+}
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Check this out, ' + myName;
+    }
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+    } else {
+        let storedName = localStorage.getItem('name');
+        myHeading.textContent = 'Check this out, ' + storedName;
+    }
